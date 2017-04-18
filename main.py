fe0056ea0f4a830e048073f42b5517aba83883b8
James Peres <james.peres.s@gmail.com>
2017-04-17 23:41:45 -0300

    update hora

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from telegram.ext import Updater, CommandHandler
-import datatime
+from datetime import datetime
+now = datetime.now()
+
 
 def start(bot, update):
-    update.message.reply_text('Hello World!')
+    update.message.reply_text('Hello World!!')
+
 
 def hello(bot, update):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
 
+
 def hora(bot, update):
-    update.message.reply_text(datatime.now())
+    update.message.reply_text(now.hour)
+    update.message.reply_text(now.minute)
 
 updater = Updater('228843118:AAGk6hkBpjIW_DazSEv843WwD_SMCuOFS0M')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('hora', hora))
 
 updater.start_polling()
 updater.idle()
